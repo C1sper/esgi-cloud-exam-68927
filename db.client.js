4f573cef779bee23f695a556e75839c9971fe95f
C1sper <arthus.pezzalidupuis@gmail.com>
2025-05-07 15:13:28 +0200

    ajout variables pour postgres

--- db.client.js
+++ db.client.js
@@ -1,22 +1,30 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  'postgres://fakeurl', // TODO
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false,
       },
     },
+    logging: false,
   },
 );
 
 // authentication and synchronization
 sequelize.authenticate()
   .then(() => {
-    sequelize.sync().catch(() => console.log("Cannot sync the database"));
+    console.log("Connexion à la base réussie.");
+    return sequelize.sync();
   })
   .catch(() => console.log("Cannot connect to database, please check environment credentials"));
 
